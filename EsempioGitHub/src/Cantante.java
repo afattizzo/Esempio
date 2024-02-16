46ab704f89a08c5e8730b72b5b391c80b0e844b9
andre <andre@PC-andrea.homenet.telecomitalia.it>
2024-02-16 19:40:48 +0100

    v1

---
+++ EsempioGitHub/src/Cantante.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+
+/**
+ * 
+ */
+public class Cantante {
+	private String nome;
+	private String nomeArte;
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getNomeArte() {
+		return nomeArte;
+	}
+	public void setNomeArte(String nomeArte) {
+		this.nomeArte = nomeArte;
+	}
+	
+}
